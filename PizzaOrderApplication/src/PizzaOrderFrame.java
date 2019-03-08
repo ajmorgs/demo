c79454ae32d03c0f98fa6e1c6a496397908035c3
ajmorgs <ajmorgs@gmail.com>
2019-03-08 14:40:42 -0500

    Order Logic complete

--- PizzaOrderApplication/src/PizzaOrderFrame.java
+++ PizzaOrderApplication/src/PizzaOrderFrame.java
@@ -8,13 +8,16 @@ import java.util.concurrent.ThreadLocalRandom;
 public class PizzaOrderFrame extends JFrame {
 
     JButton quitButton; JButton orderButton; JButton clearButton;
-
     JTextArea statusText;
-    PizzaPanel crustPanel; PizzaPanel sizePanel; PizzaPanel toppingsPanel; JPanel resultsPanel; JPanel finalPanel;
+    PizzaPanel crustPanel; PizzaPanel sizePanel; PizzaPanel toppingsPanel; JPanel resultsPanel;
     JScrollPane scrollPane;
     JCheckBox checkbox;
-
-    JLabel userLabel;JLabel computerLabel;JLabel tiesLabel;
+    JComboBox pizzaSizes;
+    ButtonGroup bg;
+    JRadioButton thinCrust;JRadioButton regularCrust;JRadioButton deepCrust;
+    String s1[] = { "Green Peppers", "Onions", "Mushrooms", "Tomato", "Anchovies","Chicken","Pineapple" };
+    StringBuilder strOrder = new StringBuilder();
+  //  Font myFont = new Font("Arial", Font.PLAIN,12);
 
     public PizzaOrderFrame() throws HeadlessException {
         createComponents();
@@ -30,11 +33,79 @@ public class PizzaOrderFrame extends JFrame {
         }
     }
 
+    private void clearOrder(){
+        pizzaSizes.setSelectedIndex(0);
+        statusText.setText("");
+        bg.clearSelection();
+        for(int x=0;x<s1.length;x++){
+            checkbox = (JCheckBox) toppingsPanel.getComponent(x);
+            checkbox.setSelected(false);
+        }
+    }
+    private void processOrder(){
+        statusText.append("==============================================\n");
+
+        double grandTotal;double tax; double taxrate=.07;double runningTotal=0;double toppingsTotal=0;
+
+        //get size
+        String psize= (String) pizzaSizes.getSelectedItem();
+
+        switch(psize){
+            case "Small":
+                runningTotal+=8;
+                break;
+            case "Medium":
+                runningTotal+=12;
+                break;
+            case "Large":
+                runningTotal+=16;
+                break;
+            case "Super":
+                runningTotal+=20;
+                break;
+        }
+        String crust="";
+        //get crust type
+        if(thinCrust.isSelected()){crust="Thin Crust";}
+        if(regularCrust.isSelected()){crust="Regular Crust";}
+        if(deepCrust.isSelected()){crust="Deep-dish";}
+        statusText.append(String.format("%-20s\t\t$%5.2f\n", psize + " " + crust +  " Pizza " , runningTotal));
+
+        String toppings="";
+        //get toppings
+        for(int x=0;x<s1.length;x++) {
+            checkbox = (JCheckBox) toppingsPanel.getComponent(x);
+            if(checkbox.isSelected()){
+                toppingsTotal+=1;
+                statusText.append(String.format("%-20s\t\t$%5.2f\n", checkbox.getText() , (double) 1.00));
+            }
+        }
+       // if(toppings!=""){
+        //remove last comma
+       //     toppings = toppings.substring(0,toppings.length()-1);
+       // }
+
+        statusText.append("\n\n");
+
+        statusText.append(String.format("%-20s\t\t$%5.2f\n","Sub-total:",runningTotal+toppingsTotal));
+//
+        //add tax
+        tax=(runningTotal+toppingsTotal)*taxrate;
+
+        statusText.append(String.format("%-20s\t\t$%5.2f\n","Tax:",tax));
+
+        statusText.append("-------------------------------------\n");
+
+        grandTotal=runningTotal+toppingsTotal+tax;
+        statusText.append(String.format("%-20s\t\t$%5.2f\n","Total:",grandTotal));
+
+    }
+
     private void createComponents() {
 
         JPanel mainPanel = new JPanel(new FlowLayout());
 
-        JComboBox pizzaSizes = new JComboBox();
+        pizzaSizes = new JComboBox();
         pizzaSizes.addItem("Small");
         pizzaSizes.addItem("Medium");
         pizzaSizes.addItem("Large");
@@ -46,14 +117,14 @@ public class PizzaOrderFrame extends JFrame {
 
         mainPanel.add(sizePanel);
 
-        JRadioButton thinCrust=new JRadioButton("Thin");
-        JRadioButton regularCrust=new JRadioButton("Regular");
-        JRadioButton deepCrust=new JRadioButton("Deep-dish");
+         thinCrust=new JRadioButton("Thin");
+         regularCrust=new JRadioButton("Regular");
+         deepCrust=new JRadioButton("Deep-dish");
         thinCrust.setBounds(75,50,100,30);
         regularCrust.setBounds(75,100,100,30);
         deepCrust.setBounds(75,150,100,30);
 
-        ButtonGroup bg=new ButtonGroup();
+        bg=new ButtonGroup();
         bg.add(thinCrust);bg.add(regularCrust);bg.add(deepCrust);
 
         crustPanel=new PizzaPanel(new FlowLayout(),"Crust Type");
@@ -66,8 +137,6 @@ public class PizzaOrderFrame extends JFrame {
 
         mainPanel.add(crustPanel);
 
-        String s1[] = { "Green Peppers", "Onions", "Mushrooms", "Tomato", "Anchovies","Chicken","Pineapple" };
-
         toppingsPanel = new PizzaPanel(new FlowLayout(),"Toppings");
 
         for(int x=0; x< s1.length; x++){
@@ -79,7 +148,7 @@ public class PizzaOrderFrame extends JFrame {
 
         add(mainPanel);
 
-        statusText = new JTextArea(10,40);
+        statusText = new JTextArea();
         statusText.setEditable(false);
         statusText.setFont(new Font("Serif", Font.ITALIC, 16));
         statusText.setLineWrap(true);
@@ -96,14 +165,14 @@ public class PizzaOrderFrame extends JFrame {
 
         orderButton = new JButton("Order");
         orderButton.setPreferredSize(new Dimension(100,80));
-      //  orderButton.addActionListener((ActionEvent event) -> exitPrompt());
+        orderButton.addActionListener((ActionEvent event) -> processOrder());
         orderButton.setMargin(new Insets(5, 5, 5, 5));
 
         mainPanel.add(orderButton);
 
         clearButton = new JButton("Clear");
         clearButton.setPreferredSize(new Dimension(100,80));
-      //  clearButton.addActionListener((ActionEvent event) -> exitPrompt());
+        clearButton.addActionListener((ActionEvent event) -> clearOrder());
         clearButton.setMargin(new Insets(5, 5, 5, 5));
 
         mainPanel.add(clearButton);
