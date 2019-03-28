a8c125db0a50a1f6aa8654df49768dfff089a853
ajmorgs <ajmorgs@gmail.com>
2019-03-28 16:42:06 -0400

    Stream of String from file

--- assignment9-students-stream/src/StudentStreamMain.java
+++ assignment9-students-stream/src/StudentStreamMain.java
@@ -1,5 +1,22 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class StudentStreamMain {
     public static void main(String[] args) {
+
+        String filename = System.getProperty("user.dir") + "\\src\\StudentsGrades.csv";
+                try (Stream<String> lineStream = Files.lines(Paths.get(filename))) {
+
+                    lineStream.forEach(System.out :: println);
+
+                }catch(IOException ex){
+                    System.out.println(ex.getMessage());
+                }
+
+
 
     }
 }
